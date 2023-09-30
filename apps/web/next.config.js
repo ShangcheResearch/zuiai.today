e9be64dcaa9af19c70fa0e191957afe9ad53980a
ranglang <rd@shangche.tech>
2023-09-30 23:39:27 +0800

    fix: uopdate

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -6,6 +6,7 @@ module.exports = {
     NEXT_PUBLIC_BACKEND_API: process.env.NEXT_PUBLIC_BACKEND_API,
   },
   serverRuntimeConfig: {
+    NEXT_PUBLIC_BACKEND_API: process.env.NEXT_PUBLIC_BACKEND_API,
   },
   publicRuntimeConfig: {
     NEXT_PUBLIC_BACKEND_API: process.env.NEXT_PUBLIC_BACKEND_API,
