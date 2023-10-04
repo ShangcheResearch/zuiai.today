0b1218a35b60b61eece0bc8ad608d812a5cd5897
Caedman Ziwen Lan <lanziwen@apitable.com>
2023-10-04 18:18:03 +0800

    fix: add file

--- apps/hocuspocus/src/index.mjs
+++ apps/hocuspocus/src/index.mjs
@@ -3,6 +3,22 @@ import { Hocuspocus } from "@hocuspocus/server";
 // Configure the server …
 const server = new Hocuspocus({
     port: 1234,
+    async onAuthenticate(data) {
+        const { token } = data;
+
+        // Example test if a user is authenticated with a token passed from the client
+        if (token !== "") {
+            throw new Error("Not authorized!");
+        }
+
+        // You can set contextual data to use it in other hooks
+        return {
+            user: {
+                id: 1234,
+                name: "John",
+            },
+        };
+    },
 });
 
 // … and run it!
